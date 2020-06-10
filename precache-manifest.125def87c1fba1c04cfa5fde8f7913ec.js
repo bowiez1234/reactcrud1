48fe52b160166d4d81993d491fee6d3a69c68765
bowiez1234@gmail.com <bowiez1234@gmail.com>
2020-06-10 19:00:13 +0700

    Updates

---
+++ precache-manifest.125def87c1fba1c04cfa5fde8f7913ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a301c7fe75b99873ae1b147e0801e8b6",
+    "url": "/reactcrud1/index.html"
+  },
+  {
+    "revision": "65d3dff29d44ac302e52",
+    "url": "/reactcrud1/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c798c56e8b6fd72419a6",
+    "url": "/reactcrud1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "65d3dff29d44ac302e52",
+    "url": "/reactcrud1/static/js/2.ba3b6be3.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/reactcrud1/static/js/2.ba3b6be3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c798c56e8b6fd72419a6",
+    "url": "/reactcrud1/static/js/main.7def10b0.chunk.js"
+  },
+  {
+    "revision": "5d1517be6ada0fc4f9e1",
+    "url": "/reactcrud1/static/js/runtime-main.7350cc8a.js"
+  }
+]);
